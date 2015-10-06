1b88c0bcf1ef9d00cc13af0170d38af61daf3165
Saber Seddik <seddik.saber@gmail.com>
2015-10-06 16:48:54 -0400

    Transform to csv

--- Groovy/src/main/groovy/com/oneobjectinc/files/ToCSV.groovy
+++ Groovy/src/main/groovy/com/oneobjectinc/files/ToCSV.groovy
@@ -1,11 +1,25 @@
 package com.oneobjectinc.files
+import au.com.bytecode.opencsv.*
 
 /**
  * Created by saberseddik on 15-10-06.
  */
 def arguments = args as List
-
-new File('/Users/saberseddik/IdeaProjects/Tutorials/Groovy/out/production/Groovy/main/resources/com/oneobjectinc/files/inputFile').eachLine {
-    println it
+def fileLocation = '/Users/saberseddik/IdeaProjects/Tutorials/Groovy/out/production/Groovy/main/resources/com/oneobjectinc/files/inputFile'
+def keyValuePattern = /([^@]*)@([^@]*)@/
+def folder =new File("/tmp/${new Date().time}/")
+folder.mkdir()
+new File(fileLocation).eachLine { line, idx ->
+    def keys=[]
+    def values=[]
+    line.findAll(keyValuePattern) { all, key, value ->
+        keys<<key
+        values<<value
+    }
+    def output=new File(folder,"${idx}.csv".toString())
+    output.createNewFile()
+    output.append(keys.join(','))
+    output.append('\n')
+    output.append(values.join(','))
 }
 
